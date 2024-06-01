b7723ff25e4f959b2005e3978c1230c05e3361b4
mstkm <mustakimmugi@gmail.com>
2024-06-01 07:38:33 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <h1>Halo</h1>
+</body>
+<script src="{{ asset('assets/bootstrap-5.3.3-dist/js/bootstrap.min.js')}}"></script>
+</html>
